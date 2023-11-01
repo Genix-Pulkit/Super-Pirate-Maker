5b5f2a10960507da9d155f6bc3586da1734d02e4
Genix-Pulkit <148672098+Genix-Pulkit@users.noreply.github.com>
2023-11-01 11:36:54 +0530

    moved to pycharm

--- code/editor.py
+++ code/editor.py
@@ -6,41 +6,52 @@ from settings import *
 from menu import Menu
 
 class Editor:
-	def __init__(self):
-		# main setup
+	def __init__(self, land_tiles):
 		self.display_surface = pygame.display.get_surface()
 		self.canvas_data = {}
-		self.grid_surf = pygame.Surface((WINDOW_WIDTH, WINDOW_HEIGHT))
-		self.grid_surf.set_colorkey('green')
-		self.grid_surf.set_alpha(60)
-
-		# navigation
+		self.land_tiles = land_tiles
 		self.origin = vector()
 		self.pan_active = False
 		self.pan_offset = vector()
-
-		# Selection Menu
+		self.support_line_surf = pygame.Surface((WINDOW_WIDTH, WINDOW_HEIGHT))
+		self.support_line_surf.set_colorkey('green')
+		self.support_line_surf.set_alpha(30)
 		self.selection_index = 2
 		self.last_selected_cell = None
-
-		# Menu
 		self.menu = Menu()
+
 
 	def get_current_cell(self):
 		distance_to_origin = vector(mouse_pos()) - self.origin
-
 		if distance_to_origin.x > 0:
 			col = int(distance_to_origin.x / TILE_SIZE)
 		else:
 			col = int(distance_to_origin.x / TILE_SIZE) - 1
-		
+
 		if distance_to_origin.y > 0:
 			row = int(distance_to_origin.y / TILE_SIZE)
 		else:
 			row = int(distance_to_origin.y / TILE_SIZE) - 1
 
 		return col, row
-		
+
+	def check_neighbors(self, cell_pos):
+		cluster_size = 3
+		local_cluster = [
+			(cell_pos[0] + col - int(cluster_size / 2), cell_pos[1] + row - int(cluster_size / 2)) 
+			for col in range(cluster_size) 
+			for row in range(cluster_size)]
+
+		for cell in local_cluster:
+			if cell in self.canvas_data:
+				self.canvas_data[cell].terrain_neighbors = []
+				for name, side in NEIGHBOR_DIRECTIONS.items():
+					neighbor_cell = (cell[0] + side[0],cell[1] + side[1])
+
+					if neighbor_cell in self.canvas_data:
+						if self.canvas_data[neighbor_cell].has_terrain:
+							self.canvas_data[cell].terrain_neighbors.append(name)
+
 
 	def event_loop(self):
 		for event in pygame.event.get():
@@ -56,9 +67,10 @@ class Editor:
 		if event.type == pygame.MOUSEBUTTONDOWN and mouse_buttons()[1]:
 			self.pan_active = True
 			self.pan_offset = vector(mouse_pos()) - self.origin
+
 		if not mouse_buttons()[1]:
 			self.pan_active = False
-		
+
 		if event.type == pygame.MOUSEWHEEL:
 			if pygame.key.get_pressed()[pygame.K_LCTRL]:
 				self.origin.y -= event.y * 50
@@ -68,13 +80,13 @@ class Editor:
 		if self.pan_active:
 			self.origin = vector(mouse_pos()) - self.pan_offset
 
-	def selection_hotkeys(self,event):
+	def selection_hotkeys(self, event):
 		if event.type == pygame.KEYDOWN:
 			if event.key == pygame.K_RIGHT:
 				self.selection_index += 1
 			if event.key == pygame.K_LEFT:
 				self.selection_index -= 1
-		self.selection_index = max(min(self.selection_index, 18), 2)
+		self.selection_index = max(2,min(self.selection_index, 18))
 
 	def menu_click(self, event):
 		if event.type == pygame.MOUSEBUTTONDOWN and self.menu.rect.collidepoint(mouse_pos()):
@@ -83,82 +95,79 @@ class Editor:
 	def canvas_add(self):
 		if mouse_buttons()[0] and not self.menu.rect.collidepoint(mouse_pos()):
 			current_cell = self.get_current_cell()
-
 			if current_cell != self.last_selected_cell:
 				if current_cell in self.canvas_data:
 					self.canvas_data[current_cell].add_id(self.selection_index)
 				else:
 					self.canvas_data[current_cell] = CanvasTile(self.selection_index)
+				self.check_neighbors(current_cell)
 				self.last_selected_cell = current_cell
 
-	def draw_grid_line(self):
+	def draw_tile_lines(self):
 		cols = WINDOW_WIDTH // TILE_SIZE
-		rows = WINDOW_HEIGHT // TILE_SIZE
-
-		origin_offset_vector = vector(
+		rows = WINDOW_HEIGHT// TILE_SIZE
+		origin_offset = vector(
 			x = self.origin.x - int(self.origin.x / TILE_SIZE) * TILE_SIZE,
-			y = self.origin.y - int(self.origin.y / TILE_SIZE) * TILE_SIZE
-		)
-
-		self.grid_surf.fill('green')
+			y = self.origin.y - int(self.origin.y / TILE_SIZE) * TILE_SIZE)
+		self.support_line_surf.fill('green')
 
 		for col in range(cols + 1):
-			x = origin_offset_vector.x + col * TILE_SIZE
-			pygame.draw.line(self.grid_surf, LINE_COLOR, (x, 0), (x, WINDOW_HEIGHT))
+			x = origin_offset.x + col * TILE_SIZE
+			pygame.draw.line(self.support_line_surf,LINE_COLOR, (x,0), (x,WINDOW_HEIGHT))
 
 		for row in range(rows + 1):
-			y = origin_offset_vector.y + row * TILE_SIZE
-			pygame.draw.line(self.grid_surf, LINE_COLOR, (0, y), (WINDOW_WIDTH, y))
+			y = origin_offset.y + row * TILE_SIZE
+			pygame.draw.line(self.support_line_surf,LINE_COLOR, (0,y), (WINDOW_WIDTH,y))
 
-		self.display_surface.blit(self.grid_surf, (0,0))
+		self.display_surface.blit(self.support_line_surf,(0,0))
 
 	def draw_level(self):
 		for cell_pos, tile in self.canvas_data.items():
 			pos = self.origin + vector(cell_pos) * TILE_SIZE
 
 			if tile.has_water:
-				test_surface = pygame.Surface((TILE_SIZE, TILE_SIZE))
-				test_surface.fill('blue')
-				self.display_surface.blit(test_surface, pos)
+				test_surf = pygame.Surface((TILE_SIZE, TILE_SIZE))
+				test_surf.fill('blue')
+				self.display_surface.blit(test_surf, pos)
 
 			if tile.has_terrain:
-				test_surface = pygame.Surface((TILE_SIZE,TILE_SIZE))
-				test_surface.fill("brown")
-				self.display_surface.blit(test_surface, pos)
+				terrain_string = ''.join(tile.terrain_neighbors)
+				terrain_style = terrain_string if terrain_string in self.land_tiles else 'X'
+				self.display_surface.blit(self.land_tiles[terrain_style], pos)
 
 			if tile.coin:
-				test_surface = pygame.Surface((TILE_SIZE,TILE_SIZE))
-				test_surface.fill("red")
-				self.display_surface.blit(test_surface, pos)
+				test_surf = pygame.Surface((TILE_SIZE, TILE_SIZE))
+				test_surf.fill('yellow')
+				self.display_surface.blit(test_surf, pos)
 
 			if tile.enemy:
-				test_surface = pygame.Surface((TILE_SIZE,TILE_SIZE))
-				test_surface.fill("yellow")
-				self.display_surface.blit(test_surface, pos)
+				test_surf = pygame.Surface((TILE_SIZE, TILE_SIZE))
+				test_surf.fill('red')
+				self.display_surface.blit(test_surf, pos)
 
 	def run(self, dt):
-		self.display_surface.fill('gray')
 		self.event_loop()
+		self.display_surface.fill('gray')
 		self.draw_level()
-		self.draw_grid_line()
+		self.draw_tile_lines()
 		pygame.draw.circle(self.display_surface, 'red', self.origin, 10)
 		self.menu.display(self.selection_index)
 
 class CanvasTile:
 	def __init__(self, tile_id):
 		self.has_terrain = False
-		self.has_terrain_neighbours = []
+		self.terrain_neighbors = []
 		self.has_water = False
-		self.water_on_Top = False
+		self.water_on_top = False
 		self.coin = None
 		self.enemy = None
 		self.objects = []
 		self.add_id(tile_id)
 
 	def add_id(self, tile_id):
-		options = {key:value['style'] for key,value in EDITOR_DATA.items()}
+		options = {key: value['style'] for key, value in EDITOR_DATA.items()}
 		match options[tile_id]:
-			case 'terrain' : self.has_terrain = True
-			case 'water' : self.has_water = True
-			case 'coin' : self.coin = tile_id
-			case 'enemy' : self.enemy = tile_id
+			case 'terrain': self.has_terrain = True
+			case 'water': self.has_water = True
+			case 'coin': self.coin = tile_id
+			case 'enemy': self.enemy = tile_id
--- code/main.py
+++ code/main.py
@@ -1,19 +1,22 @@
 import pygame
 from settings import *
+from support import *
 from pygame.image import load
-
 from editor import Editor
 
 class Main:
 	def __init__(self):
 		pygame.init()
 		self.display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
-		pygame.display.set_caption("Super Pirate Maker")
 		self.clock = pygame.time.Clock()
-		self.editor = Editor()
-		cursor_surf = load('./graphics/cursors/mouse.png').convert_alpha()
-		cursor = pygame.cursors.Cursor((0, 0), cursor_surf)
+		self.imports()
+		self.editor = Editor(self.land_tiles)
+		surf = load('../graphics/cursors/mouse.png').convert_alpha()
+		cursor = pygame.cursors.Cursor((0,0), surf)
 		pygame.mouse.set_cursor(cursor)
+
+	def imports(self):
+		self.land_tiles = import_folder_dict('../graphics/terrain/land')
 
 	def run(self):
 		while True:
